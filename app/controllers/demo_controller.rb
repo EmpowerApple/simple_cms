beb1b0b80cdfeb152f910e834ea524a03c6fc922
EmpowerApple <appledev@ecps.md>
2013-01-28 16:14:19 -0600

    second commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,14 @@
+class DemoController < ApplicationController
+  
+  layout 'admin'
+  
+  def index
+  end
+  
+  def javascript
+  end
+  
+  def text_helpers
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'd96bdebd6454d5302a2c7f2a54718b0d53a30f1b732994070d8a02a4ee389f9cf52dff34be3a410267360a1ed5c8184fa15ab4cb4e2d604e556f604821b66fb9'
